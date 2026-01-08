0b211c8ba54a62c81d683c6c0dba7d421016a3dd
adjin007 <renauldadjinda1@gmail.com>
2026-01-08 20:47:50 +0100

    Update bot_sja7.py

--- bot_sja7.py
+++ bot_sja7.py
@@ -1,14 +1,14 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
-from telegram import Update
-from telegram.ext import ContextTypes
+import asyncio
 
 TOKEN = "8376494600:AAHJedKMXU075lfANlXI1D_9N-wf_BJ17Zs"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ BOT OK")
+async def start(update, context):
+    await update.message.reply_text("✅ Bot en ligne")
 
 print("🚀 Bot en démarrage...")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+
 app.run_polling()
